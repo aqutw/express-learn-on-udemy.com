3bc2db42af0d27fcdc7a6d405b96d57fa799369d
aqutw <aquarian.ex+github@gmail.com>
2016-02-06 21:47:15 +0800

    test express redirect AND req.query

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  if (req.query && req.query.created) {
+    res.send('created '+req.query.created+'!!');
+  }
   res.send('respond with a resource');
 });
 
 router.get('/:id', function(req, res, next) {
-  res.send(req.params.id)
+  var str = '<h1>'+req.params.id+'</h1><form method=post action="/users/"><input type="text" name=username value=test_username /><input type="text" name="email" value="test_email@aaa.com" /><input type=submit value="Submit" /></form>'
+  res.send(str)
+})
+router.post('/', function(req, res, next) {
+  var username = req.body.username
+  var email = req.body.email
+  
+  //res.send(username+','+email) //show created user data
+  res.redirect('/users/?created='+username); //redirect
 })
 
 module.exports = router;
